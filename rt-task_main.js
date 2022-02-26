3198e7add093480293c6df5903c8e7a67cfc8db0
saltinaa <100444213+saltinaa@users.noreply.github.com>
2022-02-26 15:58:56 -0500

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -41,7 +41,7 @@ var fixation = {
     timeline: [toosoon],
     type: 'html-keyboard-response',
     stimulus: '<div style="font-size:60px;">+</div>',
-    choices: ' ',
+    choices: [' '],
     trial_duration: function () {
         return jsPsych.randomization.sampleWithReplacement([500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500], 1)[0];
     },
