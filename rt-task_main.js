832bc2857cd1e11a98545111911e59a01743bc2a
saltinaa <100444213+saltinaa@users.noreply.github.com>
2022-02-26 16:34:49 -0500

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -37,23 +37,15 @@ var test_stimuli = [{
 ];
 
 var fixation = {
-    timeline: [toosoon],
     type: 'html-keyboard-response',
     stimulus: '<div style="font-size:60px;">+</div>',
-    choices: [' '],
+    choices: jsPsych.NO_KEYS,
     trial_duration: function () {
         return jsPsych.randomization.sampleWithReplacement([500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500], 1)[0];
     },
     data: {
         test_part: 'fixation'
     }
-}
-
-var toosoon = {
-    timeline: [fixation]
-    type: "html-keyboard-response",
-    stimulus: 'You pressed too soon.',
-    choices: [' ']
 }
 
 var test = {
