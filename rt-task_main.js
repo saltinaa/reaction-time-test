5d2be760b5e908e0f5ddf2b40cc12a410338159e
saltinaa <100444213+saltinaa@users.noreply.github.com>
2022-02-26 15:11:41 -0500

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -45,9 +45,15 @@ var fixation = {
     trial_duration: function () {
         return jsPsych.randomization.sampleWithReplacement([500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500], 1)[0];
     },
-    data: { test_part: 'fixation' }
+    data: {
+        test_part: 'fixation',
+        early_response: ' '
+    }
+    on_finish: function (data) {
+        data.early = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.early_response);
+    },
     conditional_function: function(){
-        if (data.key_press) {
+        if (data.early != 0) {
             return true;
         } else {
             return false;
